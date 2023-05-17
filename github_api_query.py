6dae4533e7fb4cd4209e959cc00a79928b1afdff
yuxiaopeng <uxiaopeng@gmail.com>
2023-05-17 21:37:29 +0800

    add get_access_token function read api token from local

--- github_api_query.py
+++ github_api_query.py
@@ -1,8 +1,13 @@
 import requests
 
+def get_access_token():
+    with open('../access_token.txt', 'r') as f:
+        access_token = f.read().strip()
+    return access_token
+
 url = 'https://api.github.com/graphql'
 json = { 'query' : '{search(query: "LLM", type: REPOSITORY, first: 10) {edges {node {... on Repository { name description url stargazerCount forkCount}}}}}' }
-api_token = "your api token"
+api_token = get_access_token()
 headers = {'Authorization': 'token %s' % api_token}
 
 r = requests.post(url=url, json=json, headers=headers)
